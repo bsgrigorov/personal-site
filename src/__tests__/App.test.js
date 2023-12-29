4f9ffc5e8b7d474ac4b1ae60268b166db1850591
bsgrigorov <b.s.grigorov@gmail.com>
2023-12-29 14:55:10 -0700

    Fixed lint tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Borislav Grigorov");
+    expect(document.title).toBe('Borislav Grigorov');
   });
 
   it('can navigate to /about', async () => {
